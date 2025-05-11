a83c5cd2eac5aa5c4594e27599b3c301f30a572a
Armen Soghoyan <sarmen@vishup.com>
2025-05-11 21:48:46 +0400

    Add sys_reboot implementation

--- soc/wch/ch32v/soc_common.c
+++ soc/wch/ch32v/soc_common.c
@@ -10,6 +10,9 @@
 #define SAFE_ACCESS_SIG_KEY_1 (0x57)
 #define SAFE_ACCESS_SIG_KEY_2 (0xA8)
 
+// SW reset
+#define RB_SOFTWARE_RESET 0x01
+
 // unlock/relock functions are used by clock control, which is a ramfunction
 // Consider bringing them into inline version
 __ramfunc
@@ -23,4 +26,12 @@ __ramfunc
 void ch32v_sys_relock(void)
 {
 	sys_write8(0x00, CH32V_SYS_R8_SAFE_ACCESS_SIG_REG);
+}
+
+void sys_arch_reboot(int type)
+{
+	ARG_UNUSED(type);
+
+	ch32v_sys_unlock();
+	sys_write8(RB_SOFTWARE_RESET, CH32V_SYS_R8_RST_WDOG_CTRL_REG);
 }
